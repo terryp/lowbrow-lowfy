15c0628eb097fc8c370051250c3628d56421d482
manderson <manderson@leapfrogonline.com>
2014-12-16 14:20:31 -0600

    timer added

--- bin/tryit.js
+++ bin/tryit.js
@@ -1,4 +1,9 @@
 var golden = require('golden');
 var goldenDiv = require('../react/golden-div.jsx');
 
+goldenDiv.makeDiv('g-div');
+goldenDiv.makeDiv('g-div2');
+
+goldenDiv.makeTimer('timer');
+
 console.log(golden.PHI);
--- react/golden-div.jsx
+++ react/golden-div.jsx
@@ -1,2 +1,47 @@
 var React = require('react');
 
+var GoldenDiv = React.createClass({
+  render: function() {
+    return (
+      <div className="goldenDiv">
+        <span>
+        I have your golden mean right here
+        </span>
+      </div>
+    );
+  }
+});
+
+var Timer = React.createClass({
+  getInitialState: function() {
+    return {secondsElapsed: 0};
+  },
+  tick: function() {
+    this.setState({secondsElapsed: this.state.secondsElapsed + 1});
+  },
+  componentDidMount: function() {
+    this.interval = setInterval(this.tick, 1000);
+  },
+  componentWillUnmount: function() {
+    clearInterval(this.interval);
+  },
+  render: function() {
+    return (
+      <div>Seconds Elapsed: {this.state.secondsElapsed}</div>
+    );
+  }
+});
+
+module.exports.makeDiv = function(id) {
+  React.render(
+    <GoldenDiv />,
+    document.getElementById(id)
+  );
+}
+
+module.exports.makeTimer = function(id) {
+  React.render(
+    <Timer />,
+    document.getElementById(id)
+  );
+}
